47585a87ca4378d562d4a9fedc8d6df600045442
yovannyz <yovanny_jzm@hotmail.com>
2017-04-04 01:47:08 -0500

    CUS - TRATAMIENTO MEDICO

--- UPC.TP3/TP3.Web/Controllers/TratamientoMedicoController.cs
+++ UPC.TP3/TP3.Web/Controllers/TratamientoMedicoController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -50,12 +53,123 @@ namespace TP2.Web.Controllers
             return mensaje;
         }
 
-        [HttpPost]
-        public ActionResult VerDetalleExamenResultado(string idRecomendacion)
+        
+        public ActionResult Detalle(string id)
         {
-            var resultado = TResultadoExamen.Obtener(int.Parse(idRecomendacion));
+            var resultado = TResultadoExamen.Obtener(int.Parse(id));
 
-            return Json(resultado, JsonRequestBehavior.AllowGet);
+            return View(resultado);
         }
+
+        public ActionResult Imprimir(int id)
+        {
+            var Paciente = TPaciente.Obtener(id);
+            var resultado = TResultadoExamen.ObtenerPorPaciente(id);
+
+            Byte[] bytes;
+            using (var ms = new MemoryStream())
+            {
+                using (var doc = new Document())
+                {
+
+                    var writer = PdfWriter.GetInstance(doc, ms);
+
+                    doc.Open();
+                    // Creamos el tipo de Font que vamos utilizar
+                    iTextSharp.text.Font _standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+
+                    // Escribimos el encabezamiento en el documento
+                    doc.Add(new Paragraph("Tratamientos del paciente : " + Paciente.T_PERSONA.nompersona + " DNI: " + Paciente.dniPaciente));
+                    doc.Add(Chunk.NEWLINE);
+
+                    // Creamos una tabla que contendrá las partidas
+                    PdfPTable tblPrueba = new PdfPTable(6);
+                    tblPrueba.WidthPercentage = 100;
+
+                    // Configuramos el título de las columnas de la tabla
+                    PdfPCell clGrado = new PdfPCell(new Phrase("Grado", _standardFont));
+                    clGrado.BorderWidth = 0;
+                    clGrado.BorderWidthBottom = 0.30f;
+                    
+
+                    PdfPCell clPuntuacion = new PdfPCell(new Phrase("Puntuacion", _standardFont));
+                    clPuntuacion.BorderWidth = 0;
+                    clPuntuacion.BorderWidthBottom = 0.30f;
+
+                    PdfPCell clClasi = new PdfPCell(new Phrase("Clasificacion", _standardFont));
+                    clClasi.BorderWidth = 0;
+                    clClasi.BorderWidthBottom = 0.30f;
+
+                    PdfPCell clTrata = new PdfPCell(new Phrase("Tratamiento", _standardFont));
+                    clTrata.BorderWidth = 0;
+                    clTrata.BorderWidthBottom = 0.30f;
+
+                    PdfPCell clHora = new PdfPCell(new Phrase("Horario", _standardFont));
+                    clHora.BorderWidth = 0;
+                    clHora.BorderWidthBottom = 0.30f;
+
+                    PdfPCell clEnfermera = new PdfPCell(new Phrase("Enfermera", _standardFont));
+                    clEnfermera.BorderWidth = 0;
+                    clEnfermera.BorderWidthBottom = 0.30f;
+
+                    // Añadimos las celdas a la tabla
+                    tblPrueba.AddCell(clGrado);
+                    tblPrueba.AddCell(clPuntuacion);
+                    tblPrueba.AddCell(clClasi);
+                    tblPrueba.AddCell(clTrata);
+                    tblPrueba.AddCell(clHora);
+                    tblPrueba.AddCell(clEnfermera);
+
+                  
+                    foreach (var item in resultado)
+                    {
+
+                        clGrado = new PdfPCell(new Phrase(item.T_RECOMENDACION_TRATAMIENTO.gradoMedico, _standardFont));
+                        clGrado.BorderWidth = 0;
+
+                        clPuntuacion = new PdfPCell(new Phrase(item.T_RECOMENDACION_TRATAMIENTO.puntuacionTotal.ToString(), _standardFont));
+                        clPuntuacion.BorderWidth = 0;
+
+                        clClasi = new PdfPCell(new Phrase(item.T_RECOMENDACION_TRATAMIENTO.clasificacionTiss, _standardFont));
+                        clClasi.BorderWidth = 0;
+
+                        clTrata = new PdfPCell(new Phrase(item.T_RECOMENDACION_TRATAMIENTO.definicionReco, _standardFont));
+                        clTrata.BorderWidth = 0;
+
+                        clHora = new PdfPCell(new Phrase(item.fechaResultado.ToString(), _standardFont));
+                        clHora.BorderWidth = 0;
+
+                        clEnfermera= new PdfPCell(new Phrase(item.T_TRATAMIENTO_UCI.T_SOLICITUD_UCI.T_EMPLEADO.T_PERSONA.nompersona, _standardFont));
+                        clEnfermera.BorderWidth = 0;
+
+                      
+
+                        // Añadimos las celdas a la tabla
+                        tblPrueba.AddCell(clGrado);
+                        tblPrueba.AddCell(clPuntuacion);
+                        tblPrueba.AddCell(clClasi);
+                        tblPrueba.AddCell(clTrata);
+                        tblPrueba.AddCell(clHora);
+                        tblPrueba.AddCell(clEnfermera);
+
+                    }
+                    doc.Add(Chunk.NEWLINE);
+                    doc.Add(tblPrueba);
+
+
+                }
+                bytes = ms.ToArray();
+
+                Response.ClearContent();
+                Response.BinaryWrite(bytes);
+                Response.AddHeader("content-disposition", "attachment;filename=Tratamiento" + Paciente.dniPaciente+ ".pdf");
+                Response.ContentType = "application/pdf";
+                Response.Flush();
+                Response.End();
+            }
+
+            return Json("Datos exportados correctamente", JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
