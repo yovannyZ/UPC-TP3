2c8edb77974372f86aa2022bfd5cb7774363b3cb
Yovanny Zeballos Medina <yovanny_jzm@hotmail.com>
2017-04-06 13:27:21 -0500

    Mockups -  interconsulta

---
+++ UPC.TP3/TP3.Web/Controllers/InterconsultaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP2.Web.Controllers
+{
+    public class InterconsultaController : Controller
+    {
+        // GET: Interconsulta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        public ActionResult Consultas()
+        {
+            return View();
+        }
+    }
+}
